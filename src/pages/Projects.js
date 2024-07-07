d2cfec1c6b8963cc4e8c0f422bfe1a9fa605e046
Taiwo Ayoola <ayoola@glade.ng>
2024-07-08 00:02:28 +0100

    Added the projects layout

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import TopLine from "../components/TopLine";
+import { Header } from "../components/Header";
+import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
+import { IoCall } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
 
-const Projects = () => {
+function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="h-screen">
+      <div className="flex flex-col justify-between h-full">
+        <div>
+          <TopLine className={"bg-ta-blue h-4"} />
+          <Header />
+        </div>
+        <div className="relative">
+          <div className="flex flex-col md:flex-col items-center ">
+            <p className="text-[60px] text-ta-blue font-hammersmith leading-none">FEATURED WORK</p>
+          </div>
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
